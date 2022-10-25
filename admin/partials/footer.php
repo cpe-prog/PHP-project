244901992d6c562b5fd7bb5aac69804ffc239a36
yan-gajila <griangajila2@gmail.com>
2022-10-25 12:42:36 +0800

    updating

---
+++ admin/partials/footer.php
@@ -0,0 +1,10 @@
+<!-- Footer Section Starts -->
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center"><?php echo date("Y"); ?> Build and Design by Group-2 in Software Development</a></p>
+            </div>
+        </div>
+        <!-- Footer Section Ends -->
+
+    </body>
+</html>
---
+++ partials-front/footer.php
@@ -0,0 +1,18 @@
+<!-- social Section Starts Here -->
+<section class="social">
+        <div class="container text-center">
+        <p>Instructor: Virginio C Sabar Jr.</p>
+        </div>
+    </section>
+    <!-- social Section Ends Here -->
+
+    <!-- footer Section Starts Here -->
+    <section class="footer">
+        <div class="container text-center">
+            <p>Build and Design By Group-2</p>
+        </div>
+    </section>
+    <!-- footer Section Ends Here -->
+
+</body>
+</html>
