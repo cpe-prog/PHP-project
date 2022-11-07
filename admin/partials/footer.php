67d2ebd92f1c64ee632574c441dce2db190d06f1
yangajila <griangajila2@gmail.com>
2022-11-07 21:16:53 +0800

    admin footer updated

--- admin/partials/footer.php
+++ admin/partials/footer.php
@@ -1,7 +1,7 @@
 <!-- Footer Section Starts -->
 <div class="footer">
             <div class="wrapper">
-                <p class="text-center"><?php echo date("Y"); ?> Build and Design by Group-2 in Software Development</a></p>
+                <p class="text-center"><?php echo date("Y"); ?>&#169; Github | Copywrite &#169; gri@n02</a></p>
             </div>
         </div>
         <!-- Footer Section Ends -->
